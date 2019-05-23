ca0b9c9e0ff815ffc3a03e36dc79fa46438036ba
neha4777 <deshmukhneha777@gmail.com>
2019-05-23 11:12:23 +0530

    initial commit

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class HomePage {
+public void home_Page(){
+	System.out.println("home page");
+}
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,7 @@
+package com.qa.tests;
+
+public class LoginTest {
+public void Test1(){
+	System.out.println("Test1");
+}
+}
